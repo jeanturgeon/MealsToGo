01d02d08bda89d7ac9f2dc98d2471a366f7e827b
Jean Turgeon <fotojt@gmail.com>
2023-01-03 18:59:47 -0500

    added map markers for restaurants

--- src/screens/map/map.screen.js
+++ src/screens/map/map.screen.js
@@ -1,8 +1,10 @@
 import React, {useState, useEffect, useContext} from 'react';
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 import styled from 'styled-components/native';
 
 import {Search as SearchBarMap} from '../../components/map/search-bar-map.component';
+import { LocationContext } from '../../services/location/location.context';
+import { RestaurantsContext } from '../../services/restaurants/restaurants.context';
 
 const Map = styled(MapView)`
     height: 100%;
@@ -11,11 +13,47 @@ const Map = styled(MapView)`
 
 export const MapScreen = () => {
 
+    const {location} = useContext(LocationContext);
+    const {restaurants =[]} = useContext(RestaurantsContext);
+
+    const [latDelta, setLatDelta] = useState(0);
+    const {lat, lng, viewport} = location;
+
+    useEffect(() => {
+        const northeastLat = viewport.northeast.lat;
+        const southwestLat = viewport.southwest.lat;
+        
+        setLatDelta( northeastLat - southwestLat); 
+        /* to know which part of the city map to render on load (North-South distance) */
+
+    },[location, viewport]);
 
     return (
         <>
             <SearchBarMap />
-            <Map />
+            <Map 
+                region={{
+                    latitude: lat,
+                    longitude: lng,
+                    latitudeDelta: latDelta,
+                    longitudeDelta: 0.02 /* East-West zoom level */
+                }}
+            >
+                {restaurants.map((restaurant) => {
+                    return (
+                        <Marker
+                            key={restaurant.name}
+                            title={restaurant.name}
+                            coordinate={{
+                                latitude: restaurant.geometry.location.lat,
+                                longitude:restaurant.geometry.location.lng,
+                            }}
+                        />
+                            
+                        
+                    )
+                })}
+            </Map>
         </>
     )
 }
