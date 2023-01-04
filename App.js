522fa0d4bb78b0116bdf65c9111a9874a7cb5329
Jean Turgeon <fotojt@gmail.com>
2023-01-04 18:26:07 -0500

    Added basis for Firebase Auth

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
+import {useState} from 'react';
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
 import { useFonts as useOswald, Oswald_400Regular } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
+import { firebaseConfig} from './firebase.config';
 import { SafeArea } from "./src/UI/safe-area.component";
 import { theme } from "./src/UI/theme/index";
 import { Navigator } from "./src/UI/navigation/app.navigator";
 import { RestaurantContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import { AuthenticationContextProvider } from './src/services/auth/authentication.context';
+
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+ 
   const [oswaldLoaded] = useOswald({ Oswald_400Regular });
   const [latoLoaded] = useLato({ Lato_400Regular });
 
@@ -21,15 +33,17 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <SafeArea>
-          <FavouritesContextProvider>
-            <LocationContextProvider>
-              <RestaurantContextProvider>
-                <Navigator />
-              </RestaurantContextProvider>
-            </LocationContextProvider>
-          </FavouritesContextProvider>
-        </SafeArea>
+        <AuthenticationContextProvider>
+          <SafeArea>
+            <FavouritesContextProvider>
+              <LocationContextProvider>
+                <RestaurantContextProvider>
+                  <Navigator />
+                </RestaurantContextProvider>
+              </LocationContextProvider>
+            </FavouritesContextProvider>
+          </SafeArea>
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
